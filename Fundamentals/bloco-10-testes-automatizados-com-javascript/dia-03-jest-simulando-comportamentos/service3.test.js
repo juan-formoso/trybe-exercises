8d8e5c4b14bd067eb1053c46d3550b62e3b1c8db
Juan Formoso <trajano.juan@gmail.com>
2021-08-26 22:02:44 -0400

    Adiciona exercicios 10.3

---
+++ Fundamentals/bloco-10-testes-automatizados-com-javascript/dia-03-jest-simulando-comportamentos/service3.test.js
@@ -0,0 +1,23 @@
+const service = require("./service");
+describe("testando a requisição", () => {
+  service.fetchDog = jest.fn();
+  afterEach(service.fetchDog.mockReset);
+
+  test("testando requisição caso a promisse resolva", async () => {
+    service.fetchDog.mockResolvedValue("request sucess");
+
+    service.fetchDog();
+    expect(service.fetchDog).toHaveBeenCalled();
+    expect(service.fetchDog).toHaveBeenCalledTimes(1);
+    await expect(service.fetchDog()).resolves.toBe("request sucess");
+    expect(service.fetchDog).toHaveBeenCalledTimes(2);
+  });
+
+  test("testando requisição caso a promisse seja rejeitada", async () => {
+    service.fetchDog.mockRejectedValue("request failed");
+
+    expect(service.fetchDog).toHaveBeenCalledTimes(0);
+    await expect(service.fetchDog()).rejects.toMatch("request failed");
+    expect(service.fetchDog).toHaveBeenCalledTimes(1);
+  });
+});
